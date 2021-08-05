3f102bd7665632ab82195365ca53de2d73bf02ba
Mindydiane <80286982+Mindydiane@users.noreply.github.com>
2021-08-04 22:01:11 -0500

    removed extra data from server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const mysql = require("mysql2");
 const express = require("express");
-
+const db = require('./db/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -8,47 +7,34 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//connect to database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // Your MySQL username,
-    user: "root",
-    // Your SQL password
-    password: "$0Met4i#9R@d",
-    database: "emp_role",
-  },
-  console.log("Connected to the emp_role database.")
-);
-
 // Get all departments
-app.get('/api/department', (req, res) => {
-  const sql = 'SELECT * FROM department';
+app.get("/api/department", (req, res) => {
+  const sql = "SELECT * FROM department";
 
   db.query(sql, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
     }
     res.json({
-     message: 'success',
-     data: rows 
+      message: "success",
+      data: rows,
     });
   });
 });
 
 // // GET a single department
-app.get('/api/department/:id', (req, res) => {
+app.get("/api/department/:id", (req, res) => {
   const sql = `SELECT * FROM department WHERE id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
     if (err) {
-      res.status(400).json({ error: err.message});
+      res.status(400).json({ error: err.message });
       return;
     }
     res.json({
-      message: 'sucess',
-      data: row
+      message: "sucess",
+      data: row,
     });
   });
 });
@@ -104,33 +90,33 @@ app.delete("/api/department/:id", (req, res) => {
 // });
 
 // Get all employee
-app.get('/api/employee', (req, res) => {
-  const sql = 'SELECT * FROM employee';
+app.get("/api/employee", (req, res) => {
+  const sql = "SELECT * FROM employee";
 
   db.query(sql, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
     }
     res.json({
-     message: 'success',
-     data: rows 
+      message: "success",
+      data: rows,
     });
   });
 });
 
 // // GET a single employee
-app.get('/api/employee/:id', (req, res) => {
+app.get("/api/employee/:id", (req, res) => {
   const sql = `SELECT * FROM employee WHERE id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
     if (err) {
-      res.status(400).json({ error: err.message});
+      res.status(400).json({ error: err.message });
       return;
     }
     res.json({
-      message: 'sucess',
-      data: row
+      message: "sucess",
+      data: row,
     });
   });
 });
@@ -158,33 +144,33 @@ app.delete("/api/employee/:id", (req, res) => {
 });
 
 // Get all roles
-app.get('/api/roles', (req, res) => {
-  const sql = 'SELECT * FROM roles';
+app.get("/api/roles", (req, res) => {
+  const sql = "SELECT * FROM roles";
 
   db.query(sql, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
     }
     res.json({
-     message: 'success',
-     data: rows 
+      message: "success",
+      data: rows,
     });
   });
 });
 
 // // GET a single role'
-app.get('/api/roles/:id', (req, res) => {
+app.get("/api/roles/:id", (req, res) => {
   const sql = `SELECT * FROM roles WHERE id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
     if (err) {
-      res.status(400).json({ error: err.message});
+      res.status(400).json({ error: err.message });
       return;
     }
     res.json({
-      message: 'sucess',
-      data: row
+      message: "sucess",
+      data: row,
     });
   });
 });
@@ -218,5 +204,5 @@ app.use((req, res) => {
 
 //function that will start the Express.js server on port
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
